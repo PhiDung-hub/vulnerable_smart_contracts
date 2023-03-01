377b80b1a894708fe2852bbabfbdfcf3c863f70f
Phi <dungho01092002@gmail.com>
2023-03-01 11:16:48 +0800

    fix paths for lsp

--- src/BaseFactory.sol
+++ src/BaseFactory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 abstract contract BaseFactory is Ownable {
   function createInstance(address _interactor) public payable virtual returns (address);
