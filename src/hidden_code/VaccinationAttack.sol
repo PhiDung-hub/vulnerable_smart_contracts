41bad4af204308858ea3ae7ee444545f0b2bf188
Nataniel Chng <48916675+nchng002@users.noreply.github.com>
2023-03-01 13:38:23 +0800

    Update VaccinationAttack.sol

--- src/hidden_code/VaccinationAttack.sol
+++ src/hidden_code/VaccinationAttack.sol
@@ -2,23 +2,16 @@
 pragma solidity ^0.8.13;
 
 contract VaccinationAttack {
-  address public victimAddress;
-  string public victimName;
-  string public victimId;
-  mapping(address => bytes32) public credentials;
-
   event Message(string message);
+  mapping(address => bytes32) public credentials;
 
   function recordVaccination(
     address _address,
     string memory _name,
     string memory _id
   ) public returns (bytes32) {
-    victimAddress = _address;
-    victimName = _name;
-    victimId = _id;
+    emit Message("Your information has been compromised!");
     credentials[_address] = keccak256(abi.encodePacked(_address, _name, _id));
-    emit Message("Malicious contract executed!");
     return credentials[_address];
   }
 
